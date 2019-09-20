fcfbb7d6566666644790fe5c7ac3b553e1907b09
luiztauffer <luiz_tauffer@hotmail.com>
2019-09-20 12:00:49 +0300

    conversion module updated

--- giocomo-lab-to-nwb/conversion_tools/conversion_module.py
+++ giocomo-lab-to-nwb/conversion_tools/conversion_module.py
@@ -1,13 +1,9 @@
-# This script grabs electrophysiology data and stores it in .nwb files.
+# This script grabs SpikeGLX data and stores it in .nwb files.
 # authors: Luiz Tauffer and Ben Dichter
 # written for Giocomo Lab
 # ------------------------------------------------------------------------------
-from pynwb import NWBFile, NWBHDF5IO, ProcessingModule
-from pynwb.device import Device
-from pynwb.base import TimeSeries
+from nwbn_conversion_tools.ephys.acquisition.spikeglx.spikeglx import Spikeglx2NWB
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import yaml
 import numpy as np
 import os
@@ -28,9 +24,28 @@ def conversion_function(*f_sources, f_nwb, metafile, **kwargs):
     **kwargs : key, value pairs
         Extra keyword arguments.
     """
-    pass
 
+    # Load metadata from YAML file
+    with open(metafile) as f:
+        metadata = yaml.safe_load(f)
 
+    # Create extractor for SpikeGLX data
+    npx_file = f_sources[0]
+    extractor = Spikeglx2NWB(nwbfile=None, metadata=metadata, npx_file=npx_file)
+
+    # Add acquisition data
+    extractor.add_acquisition(es_name='ElectricalSeries', metadata=metadata['Ephys'])
+
+    # Run spike sorting method
+    extractor.run_spike_sorting()
+
+    # Save content to NWB file
+    extractor.save(to_path=f_nwb)
+
+    # Check file was saved and inform on screen
+    print('File saved at:')
+    print(f_nwb)
+    print('Size: ', os.stat(f_nwb).st_size/1e6, ' mb')
 
 
 #If called directly fom terminal
