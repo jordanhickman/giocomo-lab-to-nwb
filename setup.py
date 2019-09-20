20be037869d43b12d070ad8a93e3a850fac9556f
luiztauffer <luiz_tauffer@hotmail.com>
2019-09-20 12:52:36 +0300

    update names

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='giocomo_lab_to_nwb',
     version='0.1dev',
     description='tool to convert giocomo matlab data into NWB:N format',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Kristin Quick',
     author_email='kristin@scenda.io',
     #packages=['giocomo_lab_to_nwb'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['pynwb',
                       'numpy',
                       'scipy',
