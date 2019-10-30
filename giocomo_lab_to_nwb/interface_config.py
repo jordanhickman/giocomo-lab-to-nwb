ac2a54ed52babfbe4ce29f0ac53ca7d4180547bc
Kristin Quick <kristinmquick@gmail.com>
2019-10-30 06:04:27 -0400

    Added if __name__ == '__main__' functionality to conversion.py so that it would convert using the parameters listed in config.yaml so that conversion.py could be run from the terminal

--- giocomo_lab_to_nwb/interface_config.py
+++ giocomo_lab_to_nwb/interface_config.py
@@ -1,6 +1,12 @@
 import yaml
 import conversion
 
+def read_yaml(config_file='config.yaml'):
+    with open(config_file, 'r') as input_file:
+        results = yaml_as_python(input_file)
+        for experiment_info in results:
+            print('converting', experiment_info['input_file'])
+            conversion.convert(**experiment_info)
 
 def yaml_as_python(val):
     """Convert YAML to dict"""
@@ -9,8 +15,5 @@ def yaml_as_python(val):
     except yaml.YAMLError as exc:
         return exc
 
-with open('config.yaml','r') as input_file:
-    results = yaml_as_python(input_file)
-    for experiment_info in results:
-        print('converting', experiment_info['input_file'])
-        conversion.convert(**experiment_info)
+if __name__ == '__main__':
+    read_yaml()
